906a197b2eb9008940476bbe58acbe196d2908e2
unknown <pranav.1rn15is055@gmail.com>
2018-07-11 15:55:48 +0530

    Day 1 Updated

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
-from flask import Flask,render_template,Request,session
+from flask import Flask,render_template,request,session,abort
 
 app=Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('home.html')
+    return render_template('login.html')
 
-#@app.route('/')
+@app.route('/home', methods=["GET", "POST"])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        return render_template('home.html',username=username,password=password)
+
 
 app.run(debug=True)
